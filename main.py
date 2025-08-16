a9ac9fead6d4eeef75449760f0f0441290b9bf79
AveePB <mike303.py@gmail.com>
2025-08-16 13:27:08 +0200

    Algorithm updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from maze.generator import MazeGenerator
-from maze.visualizer import N_ROWS, N_COLS, TILE_SIZE, MAZE_IMG_PATH, drawMaze
+from maze.consts import N_ROWS, N_COLS, TILE_SIZE, MAZE_IMG_PATH
+import maze.generation.dfs as dfs
+import maze.generation.kruskal as kruskal
 import pygame
 
 # Constants
@@ -7,31 +8,13 @@ IS_TRAINING = True
 FPS = 1
 
 # Initialize screen
-screen = pygame.display.set_mode(((2 * N_COLS - 1) * TILE_SIZE, (2 * N_ROWS - 1) * TILE_SIZE))
+screen = pygame.display.set_mode((N_COLS * TILE_SIZE, N_ROWS * TILE_SIZE))
 pygame.display.set_icon(pygame.image.load(MAZE_IMG_PATH))
 pygame.display.set_caption('Maze')
 clock = pygame.time.Clock()
 
-# Initialize game 
-generator = MazeGenerator(N_ROWS, N_COLS)
-visited_cells = []
-is_running = True
-
-while is_running:
-    # Handle user input
-    for event in pygame.event.get():
-        # Close window
-        if event.type == pygame.QUIT:
-            is_running = False
-            break
-
-    # Fetch maze
-    maze = generator.getMaze()
-    
-    # Draw game
-    drawMaze(screen, maze, visited_cells)
-    pygame.display.flip()
-    clock.tick(FPS)
+maze = kruskal.create(screen)
+print(maze)
 
 pygame.quit()
     
--- maze/visualizer.py
+++ maze/visualizer.py
@@ -1,46 +1,32 @@
-from maze.generator import MOUSE, WALL
+from maze.consts import CHEESE, MOUSE, WALL, N_ROWS, N_COLS, TILE_SIZE, CHEESE_IMG_PATH, MOUSE_IMG_PATH
 import pygame
 
-MAZE_IMG_PATH = './assets/image/maze.png'
-CHEESE_IMG_PATH = './assets/image/cheese.png'
-MOUSE_IMG_PATH = './assets/image/mouse.png'
-
-N_ROWS = 10
-N_COLS = 10
-TILE_SIZE = 32
-
-def drawMaze(screen, maze, visited_cells):
+def drawMaze(screen, maze):
     # Draw grid
-    for row in range(len(maze)):
-        for col in range(len(maze[0])):
+    for row in range(N_ROWS):
+        for col in range(N_COLS):
             # Create tile
             rect = (col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE)
 
             # Select color
-            if maze[row][col] == MOUSE:
+            if maze[row][col] == CHEESE:
+                color = pygame.Color(252, 215, 3) # Yellow
+            elif maze[row][col] == MOUSE:
                 color = pygame.Color(102, 102, 100) # Gray
             elif maze[row][col] == WALL:
                 color = pygame.Color(204, 14, 0) # Red
+                #color = pygame.Color(0, 0, 0) # Black
             else:
                 color = pygame.Color(252, 252, 252) # White
             
-            # Draw cell
+            # Draw cell and texture
             pygame.draw.rect(screen, color, rect)
-            if maze[row][col] == MOUSE: 
+            if maze[row][col] == CHEESE: 
+                texture = pygame.image.load(CHEESE_IMG_PATH)
+                texture = pygame.transform.scale(texture, (TILE_SIZE, TILE_SIZE))
+                screen.blit(texture, rect)
+
+            elif maze[row][col] == MOUSE: 
                 texture = pygame.image.load(MOUSE_IMG_PATH)
                 texture = pygame.transform.scale(texture, (TILE_SIZE, TILE_SIZE))
                 screen.blit(texture, rect)
-    
-    # Draw visited cells
-    color = pygame.Color(42, 201, 10) # Lime
-
-    # Draw cheese
-    color = pygame.Color(252, 215, 3) # Yellow
-    x = (len(maze[0]) - 1) * TILE_SIZE
-    y = (len(maze) - 1) * TILE_SIZE
-    rect = (x, y, TILE_SIZE, TILE_SIZE)
-    pygame.draw.rect(screen, color, rect)
-
-    texture = pygame.image.load(CHEESE_IMG_PATH)
-    texture = pygame.transform.scale(texture, (TILE_SIZE, TILE_SIZE))
-    screen.blit(texture, rect)
